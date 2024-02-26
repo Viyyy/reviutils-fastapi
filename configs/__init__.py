77e133a1e627301d130d6905a9375a672fbb4d45
Viyyy <1793707472@qq.com>
2024-02-26 15:22:35 +0800

    .

--- configs/__init__.py
+++ configs/__init__.py
@@ -5,9 +5,9 @@ class WebConfig():
         with open(config_path, 'r', encoding='utf-8') as f:
             self.config = yaml.safe_load(f)
             
-        root_path = self.config['server'].get('root_path')
-        if root_path:
-            self.config['app']['openapi_url'] = root_path + '/openapi'
+        # root_path = self.config['server'].get('root_path')
+        # if root_path:
+        #     self.config['app']['openapi_url'] = root_path + '/openapi'
     
     @property
     def sqlite_db(self):
